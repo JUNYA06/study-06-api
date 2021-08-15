012fe3bfba5b752f36cff4c7c5d9b799dd579fc7
JUNYA06 <junya.mmm.jp.soccer@gmail.com>
2021-08-15 17:35:37 +0900

    0815

--- api.py
+++ api.py
@@ -11,7 +11,33 @@ def main():
     keyword = "鬼滅"
     url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?format=json&keyword={}&applicationId=1019079537947262807".format(
         keyword)
-    print(get_api(url))
+    payload = {
+    'hits': 30,#一度のリクエストで返してもらう最大個数（MAX30)
+    'page':1,#何ページ目か
+    'postageFlag':1,#送料込みの商品に限定
+    }
+    r = requests.get(url, params=payload)
+    resp = r.json()
+    total = int(resp['count'])
+    Max = total/30 + 1
+    print("【num of item】",total)
+    print("【num of page】",Max)
+    print("===================================")
+
+    counter = 0
+    for i in resp['Items']:
+        counter = counter + 1
+        item = i['Item']
+        name = item['itemName']
+        print('【No.】'+ str(counter)) 
+        print('【Name】' + str(name[:30].encode('utf-8')) + '...')
+        print('【Price】' + '¥' +str(item['itemPrice']))
+        print('【URL】',item['itemUrl'])
+        print('【shop】',item['shopName'])
+        print('【text】', item['itemCaption'])
+
+
+    # print(get_api(url))
 
 
 main()
---
+++ api2.py
@@ -0,0 +1,33 @@
+import requests
+import pandas as pd
+
+REQUEST_URL = 'https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706'
+APP_ID = '1099071000661495268'
+
+params = {
+    'applicationId' : APP_ID,
+    'format':'json',
+    'keyword':'バスタオル',
+    'minPrice':10000
+}
+
+res = requests.get(REQUEST_URL,params)
+res.status_code
+result = res.json()
+items = result['Items']
+
+df = pd.DataFrame(items)[:3]
+
+# 内包表記
+items_list = [item['Item'] for item in items]
+df_items = pd.DataFrame(items_list)[:3]
+print(df_items)
+
+
+
+
+columns = [ 'availability', 'shopAffiliateUrl',
+'itemCode',  'itemName', 'itemPrice','shopCode', 'shopName', 'reviewCount',
+ 'itemUrl']
+df = df_items[columns]
+df_items.to_csv('test.csv')
